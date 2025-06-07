29d3704ca754f91a6808dce4dd8c1a982cf3664c
Renildo15 <renildorabi@ufrn.edu.br>
2025-06-07 13:48:35 -0300

    feat: criando model stadium

--- stadium_app/models.py
+++ stadium_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
+class Stadium(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    capacity = models.PositiveIntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = "Stadium"
+        verbose_name_plural = "Stadiums"
+
+    def __str__(self):
+        return f"{self.name}"
